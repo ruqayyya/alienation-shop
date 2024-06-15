74b9cc0f0a99f501b9c506c420b21195578d1b9e
ruqayyya <badirovaruqayya@gmail.com>
2024-06-15 22:23:16 +0400

    create header

---
+++ src/components/Header/TopHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TopHeader = () => {
+  return (
+    <div className="top-header">
+      <p className="top-title">FREE SHIPPING OVER €150</p>
+    </div>
+  );
+};
+
+export default TopHeader;
