3cf140df256a84098636699ef4615e0049ad2d4f
ruqayyya <badirovaruqayya@gmail.com>
2024-07-04 00:11:46 +0400

    sidenav

--- src/components/Header/SidebarMenu.jsx
+++ src/components/Header/SidebarMenu.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import x from "../../assets/images/multiply.png";
-import Navbar from "./Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import SideNavbar from "./SideNavbar";
 
 const SidebarMenu = () => {
   return (
     <section className="Side-bar">
       <div className="container">
-        <div className="x-icon">
-          <img src={x} alt="x-icon" />
-        </div>
-        <div className="sidenav">
-          <Navbar />
-        <span className="title">account</span>
+        <div className="top">
+          <div className="x-icon">
+            <img src={x} alt="x-icon" />
+          </div>
+          <div className="sidenav">
+            <SideNavbar/>
+            <p className="title">account</p>
+          </div>
         </div>
         <div className="icon">
           <span></span>
