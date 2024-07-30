9e6b70265ce7bd0dfce80531b5131e3f04002407
ruqayyya <badirovaruqayya@gmail.com>
2024-07-31 02:04:02 +0400

    collaboration part

---
+++ src/db/collaborationDb.js
@@ -0,0 +1,67 @@
+import silvia from "../../src/assets/images/collaimage.avif";
+import nito from "../../src/assets/images/nito.avif";
+import julia from "../../src/assets/images/julia.avif";
+import riccardo from "../../src/assets/images/riccardo.avif";
+import angio from "../../src/assets/images/angio.avif";
+import glaver from "../../src/assets/images/glaver.avif";
+import jennifer from "../../src/assets/images/jennifer.avif";
+import jenesy from "../../src/assets/images/jenesy.avif";
+import alexandra from "../../src/assets/images/alexandra.avif";
+
+export const collaboration = [
+  {
+    id: 1,
+    name: "SILVIA OLIVIER",
+    mail: "@sivi_show_official",
+    img: silvia,
+  },
+  {
+    id: 2,
+    name: "NITRO",
+    mail: "@robertdenitro",
+    img: nito,
+  },
+  {
+    id: 3,
+    name: "JULIA BIZZOTTO",
+    mail: "@_.sweetdisposition._",
+    img: julia,
+  },
+  {
+    id: 4,
+    name: "RICCARDO ALDIGHIERI",
+    mail: "@iamriccardoaldi",
+    img: riccardo,
+  },
+  {
+    id: 5,
+    name: "ANGIE CORINE",
+    mail: "@angiecorine",
+    img: angio,
+  },
+  {
+    id: 6,
+    name: "CLAVER GOLD",
+    mail: "@clavergold",
+    img: glaver,
+  },
+  {
+    id: 7,
+    name: "JENNIFER PONI",
+    mail: "@stramiaoo",
+    img: jennifer,
+  },
+  {
+    id: 8,
+    name: "JENESY",
+    mail: "jenesy.js",
+    img: jenesy,
+  },
+  {
+    id: 9,
+    name: "ALEXANDRA MATEI",
+    mail: "@killwhore",
+    img: alexandra,
+  },
+
+];
